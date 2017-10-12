0a4b67bec2fdbfd7111eb5ba4140cc0d4329b655
McLeodN <31251637+mcleod805@users.noreply.github.com>
2017-10-12 10:14:44 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,30 +19,24 @@ if __name__=="__main__":
     
 @app.route("/page1")
 def render_main():
-    return render_template('page1.html')
+    return render_template('page2.html')
 
-@app.route("/response")
-def render_response():
+@app.route("/response2")
+def render_response2():
     time_convert_dict = {
         DAY: {HRS: Decimal('24')},
     }
     return render_template('response2.html', response = reply)
     
-if __name__=="__main__":
-    app.run(debug=False, port=54321)
     
-    
-    @app.route("/page2")
+    @app.route("/page3")
 def render_main():
-    return render_template('page2.html')
+    return render_template('page3.html')
 
-@app.route("/response")
-def render_response():
+@app.route("/response3")
+def render_response3():
     time_convert_dict = {
         HRS: {MIN: Decimal('60')},
     }
     return render_template('response3.html', response = reply)
-    
-if __name__=="__main__":
-    app.run(debug=False, port=54321)
 
