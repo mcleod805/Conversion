2c5eb97097cac44fc8fd4311c7687a43ae071ccd
McLeodN <31251637+mcleod805@users.noreply.github.com>
2017-10-16 09:22:33 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,9 +2,9 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/page1")
+@app.route("/")
 def render_main():
-    return render_template('page1.html')
+    return render_template('home.html')
 
 @app.route("/response1")
 def render_response1():
@@ -29,7 +29,7 @@ def render_response2():
     return render_template('response2.html', response = reply)
     
     
-    @app.route("/page3")
+@app.route("/page3")
 def render_main():
     return render_template('page3.html')
 
